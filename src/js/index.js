42549e8c68ae023e799db3d24126226ea3c1d290
Paulo Silva <progpaulosilva@gmail.com>
2024-02-25 19:22:32 -0300

    adicionado responsividade e corrigido o addEventListener no js

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,36 @@ const input = document.querySelectorAll('.input');
 const btnEnviar = document.getElementById('btn-enviar');
 
 
-
-input.forEach(function (i) {
-
-    btnEnviar.addEventListener('click', function (event) {
-        event.preventDefault();
-        if (i.value === '') {
-            i.parentNode.classList.add('requerido')
+btnEnviar.addEventListener('click', (e) => {
+    e.preventDefault()
+    input.forEach((input) => {
+        if(!input.value){
+            input.parentElement.classList.add('requerido')
+            input.parentElement.classList.remove('digitado-correto')
+        }else{
+            input.parentElement.classList.add('digitado-correto')
+            input.parentElement.classList.remove('requerido')
         }
     })
-
 })
+
+
+
+
+
+
+
+
+
+// input.forEach(function (i) {
+
+//     btnEnviar.addEventListener('click', function (event) {
+//         event.preventDefault();
+//         if (i.value === '') {
+//             i.parentNode.classList.add('requerido')
+//         }else{
+//             i.parentNode.classList.add('digitado-correto')
+//         }
+//     })
+
+// })
